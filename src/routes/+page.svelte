def41c724bfabb6a2449d857b15bd1e8ae69c610
GalLahat <gal.lahat.mail@gmail.com>
2023-11-22 15:47:53 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { ImagePlaceholder } from "flowbite-svelte";
+  import { TextPlaceholder } from "flowbite-svelte";
+</script>
+
+<ImagePlaceholder />
+<ImagePlaceholder class="mt-8" />
+
+<br />
+
+<TextPlaceholder />
+<TextPlaceholder size="md" class="mt-8" />
+<TextPlaceholder size="lg" class="mt-8" />
+<TextPlaceholder size="xl" class="mt-8" />
+<TextPlaceholder size="xxl" class="mt-8" />
